23505acd563075bfbea2879b162713f550666ba3
lorenzo <lorenzo.cerrone@uzh.ch>
2024-11-20 16:39:24 +0100

    fix: type TMembers

--- src/ome_zarr_models/zarr_models/v2.py
+++ src/ome_zarr_models/zarr_models/v2.py
@@ -2,14 +2,14 @@ from __future__ import annotations
 
 from collections.abc import Mapping
 from dataclasses import dataclass
-from typing import Any, Generic, TypeVar
+from typing import Any, Generic, TypeVar, Union
 
 TAttr = TypeVar("TAttr", bound=Mapping[str, Any])
-TMembers = TypeVar("TMembers", bound=Group | Array)
+TMembers = TypeVar("TMembers", bound=Union["Group", "Array"])
 
 
 @dataclass(kw_only=True, slots=True, frozen=True)
-class Group(Generic[TAttr, TMembers]):
+class Group(Generic[TAttr, "TMembers"]):
     attributes: TAttr
     members: Mapping[str, Group | Array]
 
