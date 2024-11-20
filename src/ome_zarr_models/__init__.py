732abd23e0510dfcee6ff4f46a99b61fcb0a2920
lorenzo <lorenzo.cerrone@uzh.ch>
2024-11-20 14:48:52 +0100

    add version to init

--- src/ome_zarr_models/__init__.py
+++ src/ome_zarr_models/__init__.py
@@ -1,2 +1,8 @@
-def hello() -> str:
-    return "Hello from ome-zarr-models-py!"
+""""""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("ome_zarr_models")
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "uninstalled"
