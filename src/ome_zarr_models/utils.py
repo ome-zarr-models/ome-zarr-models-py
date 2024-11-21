e8db5260ab3e83bc87597452e7a750df7ba3b8cb
Davis Vann Bennett <davis.v.bennett@gmail.com>
2024-11-21 14:09:15 +0100

    update utils

--- src/ome_zarr_models/utils.py
+++ src/ome_zarr_models/utils.py
@@ -1,3 +1,7 @@
+import pydantic
+from dataclasses import MISSING, fields, is_dataclass
+from pydantic import create_model
+
 from typing import TypeVar
 T = TypeVar("T")
 
@@ -6,3 +10,33 @@ def _unique_items_validator(values: list[T]) -> list[T]:
         if value in values[ind:]:
             raise ValueError(f"Non-unique values in {values}.")
     return values
+
+
+def dataclass_to_pydantic(dataclass_type: type) -> type[pydantic.BaseModel]:
+    """Convert a dataclass to a Pydantic model.
+
+    Parameters
+    ----------
+    dataclass_type : type
+        The dataclass to convert to a Pydantic model.
+    
+    Returns
+    -------
+    type[pydantic.BaseModel] a Pydantic model class.
+    """
+    if not is_dataclass(dataclass_type):
+        raise TypeError(f"{dataclass_type} is not a dataclass")
+    
+    field_definitions = {}
+    for _field in fields(dataclass_type):
+        if _field.default is not MISSING:
+            # Default value is provided
+            field_definitions[_field.name] = (_field.type, _field.default)
+        elif _field.default_factory is not MISSING:
+            # Default factory is provided
+            field_definitions[_field.name] = (_field.type, _field.default_factory())
+        else:
+            # No default value
+            field_definitions[_field.name] = (_field.type, Ellipsis)
+    
+    return create_model(dataclass_type.__name__, **field_definitions)
