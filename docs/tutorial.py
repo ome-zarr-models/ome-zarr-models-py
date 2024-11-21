7b96f0018a4d54e32e37f26e20d9ed3836b35514
Davis Vann Bennett <davis.v.bennett@gmail.com>
2024-11-21 15:28:03 +0100

    revert changes to docs

--- docs/tutorial.py
+++ docs/tutorial.py
@@ -1,6 +1,6 @@
 # # Tutorial
 
-from ome_zarr_models.v04.models
+# from ome_zarr_models.v04 import Image as OMEZarrImage
 
 # my_image = OMEZarrImage(path="path/to/ome/zarr/directory.ome.zarr")
 # print(my_image.multiscales)
