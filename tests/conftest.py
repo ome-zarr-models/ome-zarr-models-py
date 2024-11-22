ca3dc7a17cf421a5ec678213329d7a15917081d0
Davis Vann Bennett <davis.v.bennett@gmail.com>
2024-11-22 22:17:02 +0100

    basestore

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,15 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from zarr.storage import BaseStore
 import pytest
-from zarr import MemoryStore
+from zarr.storage import MemoryStore
 
 
 @pytest.fixture
-def store(request: pytest.FixtureRequest) -> MemoryStore:
+def store(request: pytest.FixtureRequest) -> BaseStore:
     match request.param:
         case "memory":
             return MemoryStore()
