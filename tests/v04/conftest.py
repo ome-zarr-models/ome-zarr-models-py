78fc53b962669b9ae3740e051b0d25f6a3b9e61b
David Stansby <dstansby@gmail.com>
2024-11-25 08:26:01 +0000

    Fix test imports

--- tests/v04/conftest.py
+++ tests/v04/conftest.py
@@ -9,11 +9,10 @@ from pydantic_zarr.v2 import ArraySpec, GroupSpec
 from zarr.util import guess_chunks
 
 from ome_zarr_models.v04.axes import Axis
+from ome_zarr_models.v04.image import Image, ImageAttrs
 from ome_zarr_models.v04.multiscales import (
     Dataset,
     Multiscale,
-    MultiscaleGroup,
-    MultiscaleGroupAttrs,
 )
 
 
@@ -63,13 +62,13 @@ def from_arrays(
     compressor: Codec | Literal["auto"] = "auto",
     fill_value: Any = 0,
     order: Literal["C", "F", "auto"] = "auto",
-) -> MultiscaleGroup:
+) -> Image:
     """
-    Create a `MultiscaleGroup` from a sequence of multiscale arrays
+    Create a `Image` from a sequence of multiscale arrays
     and spatial metadata.
 
     The arrays are used as templates for corresponding `ArraySpec` instances,
-    which model the Zarr arrays that would be created if the `MultiscaleGroup`
+    which model the Zarr arrays that would be created if the `Image`
     was stored.
 
     Parameters
@@ -143,9 +142,9 @@ def from_arrays(
         ),
         coordinateTransformations=None,
     )
-    return MultiscaleGroup(
+    return Image(
         members=GroupSpec.from_flat(members_flat).members,
-        attributes=MultiscaleGroupAttrs(multiscales=(multimeta,)),
+        attributes=ImageAttrs(multiscales=(multimeta,)),
     )
 
 
@@ -163,12 +162,12 @@ def from_array_props(
     compressor: Codec | Literal["auto"] = "auto",
     fill_value: Any = 0,
     order: Literal["C", "F"] = "C",
-) -> MultiscaleGroup:
+) -> Image:
     """
-    Create a `MultiscaleGroup` from a dtype and a sequence of shapes.
+    Create a `Image` from a dtype and a sequence of shapes.
 
     The dtype and shapes are used to parametrize `ArraySpec` instances which model the
-    Zarr arrays that would be created if the `MultiscaleGroup` was stored.
+    Zarr arrays that would be created if the `Image` was stored.
 
     Parameters
     ----------
@@ -244,7 +243,7 @@ def from_array_props(
         ),
         coordinateTransformations=None,
     )
-    return MultiscaleGroup(
+    return Image(
         members=GroupSpec.from_flat(members_flat).members,
-        attributes=MultiscaleGroupAttrs(multiscales=(multimeta,)),
+        attributes=ImageAttrs(multiscales=(multimeta,)),
     )
