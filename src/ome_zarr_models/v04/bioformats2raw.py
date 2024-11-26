a9ca4013d12ca9bbd18a5d6ea821c5c6c5989662
David Stansby <dstansby@gmail.com>
2024-11-26 08:55:35 +0000

    Update src/ome_zarr_models/v04/bioformats2raw.py
    Co-authored-by: Davis Bennett <davis.v.bennett@gmail.com>

--- src/ome_zarr_models/v04/bioformats2raw.py
+++ src/ome_zarr_models/v04/bioformats2raw.py
@@ -8,7 +8,7 @@ from ome_zarr_models.v04.plate import Plate
 
 class BioFormats2RawAttrs(Base):
     """
-    A bioformats2raw zarr group.
+    A model of the attributes contained in a bioformats2raw zarr group.
     """
 
     bioformats2raw_layout: Literal[3] = Field(..., alias="bioformats2raw.layout")
