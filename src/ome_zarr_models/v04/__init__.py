4e4cc42e09bb41ada18a0a470a7fbd5ae922eced
David Stansby <dstansby@gmail.com>
2024-11-26 08:48:55 +0000

    Don't import to top level namespace

--- src/ome_zarr_models/v04/__init__.py
+++ src/ome_zarr_models/v04/__init__.py
@@ -1,5 +1,4 @@
-from ome_zarr_models.v04.bioformats2raw import BioFormats2RawAttrs
 from ome_zarr_models.v04.hcs import HCS
 from ome_zarr_models.v04.image import Image
 
-__all__ = ["HCS", "Image", "BioFormats2RawAttrs"]
+__all__ = ["HCS", "Image"]
