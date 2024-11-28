7f6254f87bbefc466c5053b4692e0d6d23b13c77
David Stansby <dstansby@gmail.com>
2024-11-28 20:58:09 +0000

    Put back get_acquisition_path

--- src/ome_zarr_models/v04/well.py
+++ src/ome_zarr_models/v04/well.py
@@ -1,22 +1,17 @@
-from typing import Annotated
+from collections import defaultdict
+from typing import Annotated, Literal
 
 from pydantic import AfterValidator, Field
 
 from ome_zarr_models.base import Base
 from ome_zarr_models.utils import _AlphaNumericConstraint, _unique_items_validator
 
-__all__ = ["WellImage", "Well"]
+__all__ = ["Well", "WellImage"]
 
 
 class WellImage(Base):
     """
     Model for an element of `Well.images`.
-
-    Notes
-    -----
-    The NGFF image is defined in a different model
-    (`NgffImageMeta`), while the `Image` model only refers to an item of
-    `Well.images`.
 
     References
     ----------
@@ -39,4 +34,30 @@ class Well(Base):
     """
 
     images: Annotated[list[WellImage], AfterValidator(_unique_items_validator)]
-    version: str | None = Field(None, description="Version of the well specification")
+    version: Literal["0.4"] | None = Field(
+        None, description="Version of the well specification"
+    )
+
+    def get_acquisition_paths(self) -> dict[int, list[str]]:
+        """
+        Get mapping from acquisition indices to corresponding paths.
+
+        Returns
+        -------
+        Dictionary with `(acquisition index: [image_path])` key/value
+        pairs.
+
+        Raises
+        ------
+        ValueError:
+            If an element of `self.well.images` has no `acquisition` attribute.
+        """
+        acquisition_dict: dict[int, list[str]] = defaultdict(list)
+        for image in self.images:
+            if image.acquisition is None:
+                raise ValueError(
+                    "Cannot get acquisition paths for Zarr files without "
+                    "'acquisition' metadata at the well level"
+                )
+            acquisition_dict[image.acquisition].append(image.path)
+        return dict(acquisition_dict)
