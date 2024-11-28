2b882c1ce72e20dd63258ead980c35afd088234a
David Stansby <dstansby@gmail.com>
2024-11-28 23:12:11 +0000

    Use configdict

--- src/ome_zarr_models/base.py
+++ src/ome_zarr_models/base.py
@@ -1,21 +1,18 @@
-import pydantic
+from pydantic import BaseModel, ConfigDict
 
 
-class Base(pydantic.BaseModel):
+class Base(BaseModel):
     """
     The base pydantic model for all metadata classes
     """
 
-    class Config:
-        """
-        Pydantic config.
-        """
-
-        validate_assignment = True
-        extra = "allow"
+    model_config = ConfigDict(
+        validate_assignment=True,
+        extra="allow",
         # This allows fields with aliases to be populated by either
         # their alias or class attribute name
         #
         # We use this so we can handle (at least) the "bioformats2raw.version"
         # key - attributes in Python can't contain a "."
-        populate_by_name = True
+        populate_by_name=True,
+    )
