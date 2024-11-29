16dfb9b0df5021fb0f385bc43d7223ebf24cd62e
David Stansby <dstansby@gmail.com>
2024-11-29 09:26:13 +0000

    Add test for bad omero colour

--- tests/v04/test_omero.py
+++ tests/v04/test_omero.py
@@ -1,10 +1,14 @@
 import json
+import re
 from pathlib import Path
+
+import pytest
+from pydantic import ValidationError
 
 from ome_zarr_models.v04.omero import Channel, Omero, Window
 
 
-def test_load_example_json():
+def test_load_example_json() -> None:
     with open(Path(__file__).parent / "data" / "omero_example.json") as f:
         data = json.load(f)
 
@@ -25,3 +29,17 @@ def test_load_example_json():
         version="0.4",
         rdefs={"defaultT": 0, "defaultZ": 118, "model": "color"},
     )
+
+
+def test_bad_colour() -> None:
+    # Check that a non-hex colour raises a validation error
+    expected_msg = re.escape("String should match pattern '[0-9a-fA-F]{6}'")
+    with pytest.raises(ValidationError, match=expected_msg):
+        Omero(
+            channels=[
+                Channel(
+                    color="0000FP",
+                    window=Window(max=65535.0, min=0.0, start=0.0, end=1500.0),
+                )
+            ],
+        )
