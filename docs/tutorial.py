a4d75ae439a44a1c0c35b060ea0cd23299f2fce8
David Stansby <dstansby@gmail.com>
2024-12-09 22:49:31 +0000

    Slim down tutorial

--- docs/tutorial.py
+++ docs/tutorial.py
@@ -1,36 +1,37 @@
 # # Tutorial
 
+import matplotlib.pyplot as plt
 import zarr
 import zarr.storage
 from rich.pretty import pprint
 
 from ome_zarr_models.data import tutorial_data_path
 from ome_zarr_models.v04 import Image2
-# ## Creating models
+# ## Loading datasets
 #
-# We can create an Image model from a zarr group, that points to an
-# OME-zarr dataset:
+# OME-zarr datasets are just zarr groups with special metadata.
+# To open an OME-zarr dataset, we first open the zarr group, and
+# then create an image object from it. This will validate the
+# metadata.
 
-print(tutorial_data_path.absolute())
 group = zarr.open(tutorial_data_path)
 ome_zarr_image = Image2(group=group)
 pprint(ome_zarr_image)
 
-# This image contains only the zarr group. To get a copy of the attributes,
-# we can access the `.attributes` property
+# No errors, which means the metadata is valid 🎉
+#
+# ## Accessing metadata
+# To access the OME-zarr metadata, use the `.attributes` property:
 
 metadata = ome_zarr_image.attributes
 pprint(metadata)
+pprint(metadata.multiscales[0].datasets)
 
 # ## Accessing data
 #
 # Although these models do not handle reading or writing data, they do expose the zarr
-# arrays.
+# arrays. For example, to get the highest resolution image:
 
 zarr_arr = ome_zarr_image.group[metadata.multiscales[0].datasets[0].path]
 pprint(zarr_arr)
-
-# ## Not using validation
-#
-# If you want to create models that are not validated against the OME-zarr
-# specifciation, you can use the ``model_construct`` method on the models.
+plt.imshow(zarr_arr, cmap='gray')
