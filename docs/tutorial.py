ed5aca9913bcda384195ac9891755ac299101683
David Stansby <dstansby@gmail.com>
2024-12-09 22:44:41 +0000

    Slim down tutorial

--- docs/tutorial.py
+++ docs/tutorial.py
@@ -2,15 +2,10 @@
 
 import zarr
 import zarr.storage
-from pydantic import ValidationError
 from rich.pretty import pprint
 
 from ome_zarr_models.data import tutorial_data_path
 from ome_zarr_models.v04 import Image2
-from ome_zarr_models.v04.coordinate_transformations import (
-    VectorTranslation,
-)
-
 # ## Creating models
 #
 # We can create an Image model from a zarr group, that points to an
@@ -26,43 +21,6 @@ pprint(ome_zarr_image)
 
 metadata = ome_zarr_image.attributes
 pprint(metadata)
-
-# Note that this is a copy of the metadata, and editing it will not automatically
-# update the attributes stored in the zarr group. We will see how to do this later.
-
-# ## Updating models
-#
-# All the fields in the models can be updated in place. When you do this, any
-# validation on the individual field you are updating will take place.
-#
-# For example, the name for the first multiscales entry isn't very descriptive,
-# so lets update it
-
-metadata.multiscales[0].name = "A cat"
-pprint(metadata.multiscales)
-
-# One constraint in the OME-zarr spec is that the coordinate transforms have to be a
-# scale, or a scale then translation (strictly in that order). So if we try and make a
-# transformation just a translation, it will raise an error.
-
-try:
-    metadata.multiscales[0].datasets[0].coordinateTransformations = VectorTranslation(
-        type="translation", translation=[1, 2, 3]
-    )
-except ValidationError as e:
-    pprint(e)
-
-
-# This means validation happens early, allowing you to catch errors
-# before getting too far.
-
-# ## Saving modified metadata
-#
-# Once we've modified the metadata, we can save it back to the zarr group by
-# assigning to the `.attributes` property.
-
-ome_zarr_image.attributes = metadata
-pprint(ome_zarr_image.group.attrs.asdict())
 
 # ## Accessing data
 #
