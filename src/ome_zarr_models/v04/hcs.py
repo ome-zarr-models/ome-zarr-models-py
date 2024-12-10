b3a464542a7407fb1e9ad43da134a6016bfab0e0
David Stansby <dstansby@gmail.com>
2024-12-10 12:11:50 +0000

    Make HCS inherit from GroupSpec

--- src/ome_zarr_models/v04/hcs.py
+++ src/ome_zarr_models/v04/hcs.py
@@ -1,9 +1,9 @@
+from pydantic_zarr.v2 import ArraySpec, GroupSpec
+
 from ome_zarr_models.v04.plate import Plate
 
 
-class HCS:
+class HCS(GroupSpec[Plate, ArraySpec | GroupSpec]):
     """
     An OME-zarr high-content screening (HCS) dataset representing a single plate.
     """
-
-    plate: Plate
