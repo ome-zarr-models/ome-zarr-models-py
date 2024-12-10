1507bac3ee96012a3b8ce37bb1aebee5276423bd
David Stansby <dstansby@gmail.com>
2024-12-10 17:48:42 +0000

    Add WellGroup to docs

--- src/ome_zarr_models/v04/__init__.py
+++ src/ome_zarr_models/v04/__init__.py
@@ -1,5 +1,6 @@
 from ome_zarr_models.v04.hcs import HCS
 from ome_zarr_models.v04.image import Image
 from ome_zarr_models.v04.image_label import ImageLabel
+from ome_zarr_models.v04.well import WellGroup
 
-__all__ = ["HCS", "Image", "ImageLabel"]
+__all__ = ["HCS", "Image", "ImageLabel", "WellGroup"]
