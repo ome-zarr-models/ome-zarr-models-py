805ce1ef7bd3dd51058219b6263edbc9771db7ab
David Stansby <dstansby@gmail.com>
2025-01-10 09:28:45 +0000

    Replace remote labels test with local one (#116)

--- tests/v04/test_labels.py
+++ tests/v04/test_labels.py
@@ -1,15 +1,18 @@
-import zarr
-
 from ome_zarr_models.v04.image import Image
-from ome_zarr_models.v04.labels import Labels, LabelsAttrs
+from ome_zarr_models.v04.labels import LabelsAttrs
+from tests.v04.conftest import json_to_zarr_group
 
 
-def test_labels() -> None:
-    # TODO: turn this into a local test
-    group = zarr.open_group(
-        "https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.4/idr0062A/6001240.zarr", mode="r"
-    )
-    image = Image.from_zarr(group)
-    assert image.labels == Labels(
-        zarr_version=2, attributes=LabelsAttrs(labels=["0"]), members={}
-    )
+def test_image_with_labels() -> None:
+    zarr_group = json_to_zarr_group(json_fname="multiscales_example.json")
+    zarr_group.create_group("labels")
+    zarr_group["labels"].create_dataset("labels0", shape=(1, 1))
+    zarr_group["labels"].attrs.put({"labels": ["labels0"]})
+
+    zarr_group.create_dataset("0", shape=(1, 1, 1, 1))
+    zarr_group.create_dataset("1", shape=(1, 1, 1, 1))
+
+    ome_group = Image.from_zarr(zarr_group)
+    labels_group = ome_group.labels
+    assert labels_group is not None
+    assert labels_group.attributes == LabelsAttrs(labels=["labels0"])
