75f6fbd95e0f29040cc65332f9091c5dce30cf25
David Stansby <dstansby@gmail.com>
2025-02-04 17:21:19 +0000

    Use common for v05 plate

--- src/ome_zarr_models/_v05/plate.py
+++ src/ome_zarr_models/_v05/plate.py
@@ -1,4 +1,8 @@
-from ome_zarr_models.v04.plate import Acquisition, Column, Plate, Row, WellInPlate
+"""
+For reference, see the [plate section of the OME-Zarr specification](https://ngff.openmicroscopy.org/0.5/index.html#plate-md).
+"""
+
+from ome_zarr_models.common.plate import Acquisition, Column, Plate, Row, WellInPlate
 
 __all__ = [
     "Acquisition",
