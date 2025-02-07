22f7e71caf6090ebaf881c2d4ac8748e9cc00ea0
David Stansby <dstansby@gmail.com>
2025-02-07 15:06:28 +0000

    Refactor some validation code (#163)
    * Move hex constraint to validation sub-module
    
    * Move array checking to validation sub-module
    
    * Put properties above method in HCS

--- src/ome_zarr_models/v04/image.py
+++ src/ome_zarr_models/v04/image.py
@@ -7,54 +7,13 @@ from pydantic import Field, model_validator
 from pydantic_zarr.v2 import ArraySpec, GroupSpec
 
 from ome_zarr_models.base import BaseAttrs
+from ome_zarr_models.common.validation import check_array_path
 from ome_zarr_models.v04.base import BaseGroupv04
 from ome_zarr_models.v04.labels import Labels
 from ome_zarr_models.v04.multiscales import Multiscale
 from ome_zarr_models.v04.omero import Omero
 
 __all__ = ["Image", "ImageAttrs"]
-
-
-def _check_arrays_compatible(data: Image) -> Image:
-    """
-    Check that all the arrays referenced by the `multiscales` metadata meet the
-    following criteria:
-        - they exist
-        - they are not groups
-        - they have dimensionality consistent with the number of axes defined in the
-          metadata.
-    """
-    multimeta = data.attributes.multiscales
-    flat_self = data.to_flat()
-
-    for multiscale in multimeta:
-        multiscale_ndim = len(multiscale.axes)
-        for dataset in multiscale.datasets:
-            try:
-                maybe_arr: ArraySpec | GroupSpec = flat_self[
-                    "/" + dataset.path.lstrip("/")
-                ]
-                if isinstance(maybe_arr, GroupSpec):
-                    msg = f"The node at {dataset.path} is a group, not an array."
-                    raise ValueError(msg)
-                arr_ndim = len(maybe_arr.shape)
-
-                if arr_ndim != multiscale_ndim:
-                    msg = (
-                        f"The multiscale metadata has {multiscale_ndim} axes "
-                        "which does not match the dimensionality of the array "
-                        f"found in this group at {dataset.path} ({arr_ndim}). "
-                        "The number of axes must match the array dimensionality."
-                    )
-
-                    raise ValueError(msg)
-            except KeyError as e:
-                msg = (
-                    f"The multiscale metadata references an array that does not "
-                    f"exist in this group: {dataset.path}"
-                )
-                raise ValueError(msg) from e
-    return data
 
 
 class ImageAttrs(BaseAttrs):
@@ -72,38 +31,10 @@ class ImageAttrs(BaseAttrs):
     omero: Omero | None = None
 
 
-def _check_array_path(group: zarr.Group, array_path: str) -> ArraySpec:
-    """
-    Check if an array exists at a given path in a group.
-
-    If it doesn't, raise a ValueError.
-    If it does, return the array spec.
-    """
-    try:
-        array = zarr.open_array(store=group.store, path=array_path, mode="r")
-        array_spec = ArraySpec.from_zarr(array)
-    except zarr.errors.ArrayNotFoundError as e:
-        msg = (
-            f"Expected to find an array at {array_path}, "
-            "but no array was found there."
-        )
-        raise ValueError(msg) from e
-    except zarr.errors.ContainsGroupError as e:
-        msg = (
-            f"Expected to find an array at {array_path}, "
-            "but a group was found there instead."
-        )
-        raise ValueError(msg) from e
-
-    return array_spec
-
-
 class Image(GroupSpec[ImageAttrs, ArraySpec | GroupSpec], BaseGroupv04):  # type: ignore[misc]
     """
     An OME-Zarr multiscale dataset.
     """
-
-    _check_arrays_compatible = model_validator(mode="after")(_check_arrays_compatible)
 
     @classmethod
     def from_zarr(cls, group: zarr.Group) -> Self:
@@ -123,7 +54,7 @@ class Image(GroupSpec[ImageAttrs, ArraySpec | GroupSpec], BaseGroupv04):  # type
         for multiscale in multi_meta.multiscales:
             for dataset in multiscale.datasets:
                 array_path = f"{group.path}/{dataset.path}"
-                array_spec = _check_array_path(group, array_path)
+                array_spec = check_array_path(group, array_path)
                 members_tree_flat["/" + dataset.path] = array_spec
 
         try:
@@ -138,6 +69,48 @@ class Image(GroupSpec[ImageAttrs, ArraySpec | GroupSpec], BaseGroupv04):  # type
             update={"members": members_normalized.members}
         )
         return cls(**group_spec.model_dump())
+
+    @model_validator(mode="after")
+    def check_arrays_compatible(self) -> Self:
+        """
+        Check that all the arrays referenced by the `multiscales` metadata meet the
+        following criteria:
+            - they exist
+            - they are not groups
+            - they have dimensionality consistent with the number of axes defined in the
+              metadata.
+        """
+        multimeta = self.attributes.multiscales
+        flat_self = self.to_flat()
+
+        for multiscale in multimeta:
+            multiscale_ndim = len(multiscale.axes)
+            for dataset in multiscale.datasets:
+                try:
+                    maybe_arr: ArraySpec | GroupSpec = flat_self[
+                        "/" + dataset.path.lstrip("/")
+                    ]
+                    if isinstance(maybe_arr, GroupSpec):
+                        msg = f"The node at {dataset.path} is a group, not an array."
+                        raise ValueError(msg)
+                    arr_ndim = len(maybe_arr.shape)
+
+                    if arr_ndim != multiscale_ndim:
+                        msg = (
+                            f"The multiscale metadata has {multiscale_ndim} axes "
+                            "which does not match the dimensionality of the array "
+                            f"found in this group at {dataset.path} ({arr_ndim}). "
+                            "The number of axes must match the array dimensionality."
+                        )
+
+                        raise ValueError(msg)
+                except KeyError as e:
+                    msg = (
+                        f"The multiscale metadata references an array that does not "
+                        f"exist in this group: {dataset.path}"
+                    )
+                    raise ValueError(msg) from e
+        return self
 
     @property
     def labels(self) -> Labels | None:
