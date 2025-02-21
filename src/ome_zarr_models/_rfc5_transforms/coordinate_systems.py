25d642d4705f50e4540a6d80014165ddd2a83bc1
Wouter-Michiel Vierdag <michiel.vierdag@embl.de>
2025-02-21 15:15:54 +0100

    parse Identity
    Co-authored-by: m.lucalmer@gmail.com

--- src/ome_zarr_models/_rfc5_transforms/coordinate_systems.py
+++ src/ome_zarr_models/_rfc5_transforms/coordinate_systems.py
@@ -1,7 +1,6 @@
-from pydantic import Field
-
 from ome_zarr_models.base import BaseAttrs
 from ome_zarr_models.common.axes import Axes
+
 
 class CoordinateSystem(BaseAttrs):
     name: str
--- tests/_rfc5_transforms/test_transformations.py
+++ tests/_rfc5_transforms/test_transformations.py
@@ -1,5 +1,5 @@
-from tests._rfc5_transforms.conftest import read_in_json
 from ome_zarr_models._rfc5_transforms.coordinate_transformations import SpatialMapper
+from tests._rfc5_transforms.conftest import read_in_json
 
 
 def test_identity_transform():
