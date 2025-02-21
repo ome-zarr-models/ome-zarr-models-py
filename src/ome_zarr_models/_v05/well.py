75c3c198b280895f82ba9b8a6e5524ce4eab3f95
David Stansby <dstansby@gmail.com>
2025-02-21 15:51:13 +0000

    Fix well version in 0.5

--- src/ome_zarr_models/_v05/well.py
+++ src/ome_zarr_models/_v05/well.py
@@ -1,7 +1,7 @@
 from pydantic_zarr.v2 import ArraySpec, GroupSpec
 
 from ome_zarr_models._v05.base import BaseGroupv05, BaseOMEAttrs, BaseZarrAttrs
-from ome_zarr_models.common.well_types import WellMeta
+from ome_zarr_models._v05.well_types import WellMeta
 
 __all__ = ["Well", "WellAttrs"]
 
