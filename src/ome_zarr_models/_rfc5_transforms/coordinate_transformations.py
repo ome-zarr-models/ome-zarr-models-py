6c55e11fb675d6e94efdb09302d23908ed6f4dba
Wouter-Michiel Vierdag <michiel.vierdag@embl.de>
2025-02-21 13:53:37 +0100

    add model validator

--- src/ome_zarr_models/_rfc5_transforms/coordinate_transformations.py
+++ src/ome_zarr_models/_rfc5_transforms/coordinate_transformations.py
@@ -1,6 +1,6 @@
 from typing import Literal
 
-from pydantic import Field
+from pydantic import Field, model_validator
 
 from ome_zarr_models.base import BaseAttrs
 
@@ -22,12 +22,22 @@ class Identity(CoordinateTransformation):
     type: Literal["identity"]
 
 
+# TODO: agree on the naming of this class
 class SpatialMapper:
     """Container class for coordinate systems and coordinate transformations."""
 
-    coordinate_transformations: list[CoordinateTransformation] = Field(
-        ..., description="List of coordinate transformations.", min_length=1
-    )
     coordinate_systems: list[CoordinateSystem] = Field(
         ..., description="List of coordinate systems.", min_length=1
     )
+    coordinate_transformations: list[CoordinateTransformation] = Field(
+        ..., description="List of coordinate transformations.", min_length=1
+    )
+
+    @model_validator(mode="after")
+    def check_cs_input_output(self):
+        """Check input and output for each coordinate system.
+
+        The input and output must either be a path relative to the top level of the zarr store
+        or must be a name that is present in the list of coordinate systems.
+        """
+        pass
