8557da827876a273cd9f10fe01feeeee17b54807
David Stansby <dstansby@gmail.com>
2025-07-23 10:15:02 +0100

    Fix typing from zarr 3

--- tests/v04/test_labels.py
+++ tests/v04/test_labels.py
@@ -6,7 +6,7 @@ from tests.v04.conftest import json_to_zarr_group
 def test_image_with_labels() -> None:
     zarr_group = json_to_zarr_group(json_fname="multiscales_example.json")
     zarr_group.create_group("labels")
-    zarr_group["labels"].create_array("labels0", shape=(1, 1), dtype="uint8")
+    zarr_group["labels"].create_array("labels0", shape=(1, 1), dtype="uint8")  # type: ignore[union-attr]
     zarr_group["labels"].attrs.put({"labels": ["labels0"]})
 
     zarr_group.create_array("0", shape=(1, 1, 1, 1), dtype="uint8")
