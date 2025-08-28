050214c9362d2a7c0ed7ca24b7f87f8e98723873
David Stansby <dstansby@gmail.com>
2025-08-28 11:27:34 +0100

    Test on unlistable store (#242)

--- tests/v05/conftest.py
+++ tests/v05/conftest.py
@@ -4,6 +4,7 @@ from typing import TypeVar
 
 import zarr
 import zarr.storage
+from zarr.abc.store import Store
 
 from ome_zarr_models.base import BaseAttrs
 
@@ -18,11 +19,11 @@ def read_in_json(*, json_fname: str, model_cls: type[T]) -> T:
         return model_cls.model_validate_json(f.read())
 
 
-def json_to_zarr_group(*, json_fname: str) -> zarr.Group:
+def json_to_zarr_group(*, json_fname: str, store: Store) -> zarr.Group:
     """
     Create an empty Zarr group, and set attributes from a JSON file.
     """
-    group = zarr.open_group(store=zarr.storage.MemoryStore(), zarr_format=3)
+    group = zarr.open_group(store=store, zarr_format=3)
     with open(Path(__file__).parent / "data" / json_fname) as f:
         attrs = json.load(f)
 
