395c30ee464bfab12a61951840ab0dea0be2ec08
David Stansby <dstansby@gmail.com>
2025-08-28 14:07:36 +0100

    Make v04 labels work with unlistable stores (#243)
    * Make v04 labels work with unlistable stores
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/v04/test_labels.py
+++ tests/v04/test_labels.py
@@ -1,16 +1,11 @@
-import pytest
 from zarr.abc.store import Store
 
 from ome_zarr_models.v04.image import Image
 from ome_zarr_models.v04.labels import LabelsAttrs
-from tests.conftest import UnlistableStore
 from tests.v04.conftest import json_to_zarr_group
 
 
 def test_image_with_labels(store: Store) -> None:
-    if isinstance(store, UnlistableStore):
-        pytest.xfail("Labels do not work on unlistable stores")
-
     zarr_group = json_to_zarr_group(json_fname="multiscales_example.json", store=store)
     zarr_group.create_group("labels")
     zarr_group["labels"].create_array("labels0", shape=(1, 1), dtype="uint8")  # type: ignore[union-attr]
