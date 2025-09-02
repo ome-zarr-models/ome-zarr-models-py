cffbf6a6f29a1e6d45a2330f5ea292a6820b3b56
David Stansby <dstansby@gmail.com>
2025-09-02 08:55:11 +0100

    Implement RFC4

--- src/ome_zarr_models/_v06/multiscales.py
+++ src/ome_zarr_models/_v06/multiscales.py
@@ -1,3 +1,8 @@
+from typing import Self
+
+from pydantic import model_validator
+
+from ome_zarr_models._v06.axes import Axes, Orientation
 from ome_zarr_models.common.multiscales import Dataset, MultiscaleBase
 
 __all__ = ["Dataset", "Multiscale"]
@@ -7,3 +12,55 @@ class Multiscale(MultiscaleBase):
     """
     An element of multiscales metadata.
     """
+
+    axes: Axes
+
+    @model_validator(mode="after")
+    def _ensure_valid_orientations(self: Self) -> Self:
+        """
+        Validate anatomical orientations.
+        """
+        orientations: list[Orientation] = [
+            a.anatomicalOrientation
+            for a in self.axes
+            if a.anatomicalOrientation is not None
+        ]
+        _check_only_one_value(
+            orientations=orientations, values=["right-to-left", "left-to-right"]
+        )
+        _check_only_one_value(
+            orientations=orientations,
+            values=["anterior-to-posterior", "posterior-to-anterior"],
+        )
+        _check_only_one_value(
+            orientations=orientations,
+            values=[
+                "inferior-to-superior",
+                "superior-to-inferior",
+                "dorsal-to-ventral",
+                "ventral-to-dorsal",
+                "dorsal-to-palmar",
+                "palmar-to-dorsal",
+                "dorsal-to-plantar",
+                "plantar-to-dorsal",
+                "rostral-to-caudal",
+                "caudal-to-rostral",
+                "cranial-to-caudal",
+                "caudal-to-cranial",
+                "proximal-to-distal",
+                "distal-to-proximal",
+            ],
+        )
+        return self
+
+
+def _check_only_one_value(
+    *, orientations: list[Orientation], values: list[Orientation]
+) -> None:
+    counter = 0
+    for value in values:
+        if value in orientations:
+            counter += 1
+
+    if counter > 1:
+        raise ValueError(f"Only one of {values} allowed in a set of axes.")
