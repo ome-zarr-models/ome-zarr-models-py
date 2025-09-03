e9db541b0ae3a696a40d2cbe38c0e6aebdbc37b9
Luca Marconato <m.lucalmer@gmail.com>
2025-09-03 18:08:51 +0200

    improved tests

--- tests/_v06/test_image.py
+++ tests/_v06/test_image.py
@@ -1,3 +1,7 @@
+import re
+
+import pytest
+from pydantic import ValidationError
 from zarr.abc.store import Store
 
 from ome_zarr_models._v06.axes import Axis
@@ -6,8 +10,9 @@ from ome_zarr_models._v06.coordinate_transformations import (
     VectorScale,
 )
 from ome_zarr_models._v06.image import Image, ImageAttrs
+from ome_zarr_models._v06.labels import LabelsAttrs
 from ome_zarr_models._v06.multiscales import Dataset, Multiscale
-from tests._v06.conftest import json_to_zarr_group
+from tests._v06.conftest import json_to_zarr_group, json_to_dict
 
 
 def test_image(store: Store) -> None:
@@ -114,4 +119,164 @@ def test_image(store: Store) -> None:
     assert ome_group.attributes.ome == image_attrs
 
 
-# TODO: copy over and adapt additional tests from v05
+# TODO: this test was copy-pasted from v05 and needs to be adjusted
+# def test_image_no_dim_names(store: Store) -> None:
+#     zarr_group = json_to_zarr_group(json_fname="image_example.json", store=store)
+#     zarr_group.create_array(
+#         "0",
+#         shape=(1, 1, 1, 1, 1),
+#         dtype="uint8",
+#         dimension_names=["t", "c", "z", "y", "x"],
+#     )
+#     arr1 = zarr_group.create_array("1", shape=(1, 1, 1, 1, 1), dtype="uint8")
+#     assert arr1.metadata.dimension_names is None  # type: ignore[union-attr]
+#     zarr_group.create_array(
+#         "2",
+#         shape=(1, 1, 1, 1, 1),
+#         dtype="uint8",
+#         dimension_names=["t", "c", "z", "y", "x"],
+#     )
+#     with pytest.raises(
+#         ValidationError,
+#         match="The array in this group at  '1' has no dimension_names metadata",
+#     ):
+#         Image.from_zarr(zarr_group)
+
+
+# TODO: this test was copy-pasted from v05 and needs to be adjusted
+# def test_image_wrong_dim_names(store: Store) -> None:
+#     zarr_group = json_to_zarr_group(json_fname="image_example.json", store=store)
+#     zarr_group.create_array(
+#         "0",
+#         shape=(1, 1, 1, 1, 1),
+#         dtype="uint8",
+#         dimension_names=["t", "c", "z", "y", "x"],
+#     )
+#     zarr_group.create_array(
+#         "1",
+#         shape=(1, 1, 1, 1, 1),
+#         dtype="uint8",
+#         dimension_names=["t", "c", "z", "x", "y"],
+#     )
+#     zarr_group.create_array(
+#         "2",
+#         shape=(1, 1, 1, 1, 1),
+#         dtype="uint8",
+#         dimension_names=["t", "c", "z", "y", "x"],
+#     )
+#     with pytest.raises(
+#         ValidationError,
+#         match=re.escape(
+#             "The multiscale metadata has ('t', 'c', 'z', 'y', 'x') axes names "
+#             "which does not match the dimension names of the array "
+#             "found in this group at path '1' (('t', 'c', 'z', 'x', 'y'))"
+#         ),
+#     ):
+#         Image.from_zarr(zarr_group)
+
+
+def test_image_with_labels(store: Store) -> None:
+    zarr_group = json_to_zarr_group(json_fname="image_example.json", store=store)
+    zarr_group.create_array(
+        "0",
+        shape=(1, 1, 1, 1, 1),
+        dtype="uint8",
+        dimension_names=["t", "c", "z", "y", "x"],
+    )
+    zarr_group.create_array(
+        "1",
+        shape=(1, 1, 1, 1, 1),
+        dtype="uint8",
+        dimension_names=["t", "c", "z", "y", "x"],
+    )
+    zarr_group.create_array(
+        "2",
+        shape=(1, 1, 1, 1, 1),
+        dtype="uint8",
+        dimension_names=["t", "c", "z", "y", "x"],
+    )
+    # Add labels group
+    labels_group = zarr_group.create_group(
+        "labels",
+        attributes=json_to_dict(json_fname="labels_example.json"),
+    )
+
+    with pytest.raises(
+        ValueError, match="Label path 'cell_space_segmentation' not found in zarr group"
+    ):
+        Image.from_zarr(zarr_group)
+
+    # Add image labels group
+    image_label_group = labels_group.create_group(
+        "cell_space_segmentation",
+        attributes=json_to_dict(json_fname="image_label_example.json"),
+    )
+    image_label_group.create_array(
+        "0",
+        shape=(1, 1, 1, 1, 1),
+        dtype="uint8",
+        dimension_names=["t", "c", "z", "y", "x"],
+    )
+    image_label_group.create_array(
+        "1",
+        shape=(1, 1, 1, 1, 1),
+        dtype="uint8",
+        dimension_names=["t", "c", "z", "y", "x"],
+    )
+    image_label_group.create_array(
+        "2",
+        shape=(1, 1, 1, 1, 1),
+        dtype="uint8",
+        dimension_names=["t", "c", "z", "y", "x"],
+    )
+    image = Image.from_zarr(zarr_group)
+    assert image.labels is not None
+    assert image.labels.attributes.ome == LabelsAttrs(
+        version="0.6", labels=["cell_space_segmentation"]
+    )
+
+
+def test_image_with_labels_mismatch_multiscales(store: Store) -> None:
+    zarr_group = json_to_zarr_group(json_fname="image_example.json", store=store)
+    zarr_group.create_array(
+        "0",
+        shape=(1, 1, 1, 1, 1),
+        dtype="uint8",
+        dimension_names=["t", "c", "z", "y", "x"],
+    )
+    zarr_group.create_array(
+        "1",
+        shape=(1, 1, 1, 1, 1),
+        dtype="uint8",
+        dimension_names=["t", "c", "z", "y", "x"],
+    )
+    zarr_group.create_array(
+        "2",
+        shape=(1, 1, 1, 1, 1),
+        dtype="uint8",
+        dimension_names=["t", "c", "z", "y", "x"],
+    )
+    # Add labels group
+    labels_group = zarr_group.create_group(
+        "labels",
+        attributes=json_to_dict(json_fname="labels_example.json"),
+    )
+    # Add image labels group
+    image_label_group = labels_group.create_group(
+        "cell_space_segmentation",
+        attributes=json_to_dict(json_fname="labels_image_example.json"),
+    )
+    image_label_group.create_array(
+        "0",
+        shape=(1, 1, 1, 1, 1),
+        dtype="uint8",
+        dimension_names=["t", "c", "z", "y", "x"],
+    )
+    with pytest.raises(
+        RuntimeError,
+        match=re.escape(
+            "Number of image label multiscale levels (1) doesn't match "
+            "number of image multiscale levels (3)."
+        ),
+    ):
+        Image.from_zarr(zarr_group)
--- tests/_v06/test_multiscales.py
+++ tests/_v06/test_multiscales.py
@@ -239,5 +239,198 @@ def test_ensure_ordered_scales() -> None:
             ),
         )
 
-
-# TODO: copy over and adapt additional tests from v05
+# TODO: all these tests have been copied over from v05, and needs to be adjusted
+# def test_ordered_multiscales() -> None:
+#     """
+#     > The "path"s MUST be ordered from largest (i.e. highest resolution) to smallest.
+#     """
+#     axes = (
+#         Axis(name="c", type="channel", unit=None),
+#         Axis(name="z", type="space", unit="meter"),
+#         Axis(name="x", type="space", unit="meter"),
+#         Axis(name="y", type="space", unit="meter"),
+#     )
+#     datasets = (
+#         Dataset(
+#             path="0",
+#             coordinateTransformations=(VectorScale(type="scale", scale=(2, 2, 2, 2)),),
+#         ),
+#         Dataset(
+#             path="1",
+#             coordinateTransformations=(VectorScale(type="scale", scale=(2, 2, 1, 2)),),
+#         ),
+#     )
+#     with pytest.raises(
+#         ValidationError,
+#         match=re.escape(
+#             "Dataset 0 has a lower resolution (scales = [2.0, 2.0, 2.0, 2.0]) "
+#             "than dataset 1 (scales = [2.0, 2.0, 1.0, 2.0])"
+#         ),
+#     ):
+#         Multiscale(
+#             axes=axes,
+#             datasets=datasets,
+#         )
+#
+#
+# def test_multiscale_group_datasets_ndim() -> None:
+#     """
+#     Test that creating a Image with arrays with mismatched shapes raises
+#     an exception
+#     > The length of "axes" ... MUST be equal to the dimensionality of the zarr arrays
+#     > storing the image data
+#     """
+#     true_ndim = 2
+#     bad_ndim = 3
+#     match = (
+#         "Length of arrays (got len(array_specs)=3) must be the same as "
+#         "length of paths (got len(paths)=2)"
+#     )
+#     with pytest.raises(ValueError, match=re.escape(match)):
+#         Image.new(
+#             array_specs=[ArraySpec.from_array(np.arange(10)) for _ in range(bad_ndim)],
+#             paths=[str(i) for i in range(true_ndim)],
+#             axes=(Axis(name="x", type="space"), Axis(name="y", type="space")),
+#             scales=((1, 1), (2, 2)),
+#             translations=((0, 0), (0.5, 0.5)),
+#         )
+#
+#
+# def test_multiscale_group_missing_arrays() -> None:
+#     """
+#     Test that creating a multiscale group fails when an expected Zarr array is missing
+#     """
+#     arrays = (
+#         zarr.zeros((10, 10)),
+#         zarr.zeros((5, 5)),
+#     )
+#     array_names = ("s0", "s1")
+#     array_specs: list[AnyArraySpec] = [
+#         ArraySpec.from_array(a, dimension_names=["x", "y"]) for a in arrays
+#     ]
+#     group_model = Image.new(
+#         array_specs=array_specs,
+#         axes=(Axis(name="x", type="space"), Axis(name="y", type="space")),
+#         paths=array_names,
+#         scales=((1, 1), (2, 2)),
+#         translations=((0, 0), (0.5, 0.5)),
+#     )
+#     # remove an array, then re-create the model
+#     assert group_model.members is not None
+#     group_model_broken = group_model.model_copy(
+#         update={"members": {array_names[0]: group_model.members[array_names[0]]}}
+#     )
+#     with pytest.raises(
+#         ValidationError,
+#         match=(
+#             "The multiscale metadata references an array that does not exist in this "
+#         ),
+#     ):
+#         Image(**group_model_broken.model_dump())
+#
+#
+# def test_multiscale_group_ectopic_group() -> None:
+#     """
+#     Test that creating a multiscale group fails when an expected Zarr array
+#     is actually a group
+#     """
+#     arrays = (
+#         zarr.zeros((10, 10)),
+#         zarr.zeros((5, 5)),
+#     )
+#     array_names = ("s0", "s1")
+#     group_model = Image.new(
+#         array_specs=[
+#             ArraySpec.from_array(a, dimension_names=["x", "y"]) for a in arrays
+#         ],
+#         axes=(Axis(name="x", type="space"), Axis(name="y", type="space")),
+#         paths=array_names,
+#         scales=((1, 1), (2, 2)),
+#         translations=((0, 0), (0.5, 0.5)),
+#     )
+#     # remove an array, then re-create the model
+#     group_model_broken = group_model.model_copy(
+#         update={"members": {array_names[0]: GroupSpec()}}
+#     )
+#     with pytest.raises(
+#         ValidationError,
+#         match=re.escape(f"The node at {array_names[0]} is a group, not an array."),
+#     ):
+#         Image(**group_model_broken.model_dump())
+#
+#
+# def test_from_zarr_missing_metadata(
+#     store: Store,
+#     request: pytest.FixtureRequest,
+# ) -> None:
+#     group_model: AnyGroupSpec = GroupSpec(attributes={"ome": {}})
+#     group = group_model.to_zarr(store, path="test")
+#     # store_path = store.path if hasattr(store, "path") else ""
+#     match = "multiscales\n  Field required"
+#     with pytest.raises(ValidationError, match=match):
+#         Image.from_zarr(group)
+#
+#
+# def test_from_zarr_missing_array(store: Store) -> None:
+#     """
+#     Test that creating a multiscale Group fails when an expected Zarr array is missing
+#     or is a group instead of an array
+#     """
+#     arrays = np.zeros((10, 10)), np.zeros((5, 5))
+#     group_path = "broken"
+#     arrays_names = ("s0", "s1")
+#     group_model = Image.new(
+#         array_specs=[
+#             ArraySpec.from_array(a, dimension_names=["x", "y"]) for a in arrays
+#         ],
+#         axes=(Axis(name="x", type="space"), Axis(name="y", type="space")),
+#         paths=arrays_names,
+#         scales=((1, 1), (2, 2)),
+#         translations=((0, 0), (0.5, 0.5)),
+#     )
+#
+#     # make an untyped model, and remove an array before serializing
+#     removed_array_path = arrays_names[0]
+#     model_dict = group_model.model_dump(exclude={"members": {removed_array_path: True}})
+#     broken_group = GroupSpec(**model_dict).to_zarr(store=store, path=group_path)
+#     match = "Expected to find an array at broken/s0, but no array was found there"
+#     with pytest.raises(ValueError, match=match):
+#         Image.from_zarr(broken_group)
+#
+#
+# def test_from_zarr_ectopic_group(store: Store) -> None:
+#     """
+#     Test that creating a multiscale Group fails when an expected Zarr array is missing
+#     or is a group instead of an array
+#     """
+#     arrays = np.zeros((10, 10)), np.zeros((5, 5))
+#     group_path = "broken"
+#     arrays_names = ("s0", "s1")
+#     group_model = Image.new(
+#         array_specs=[
+#             ArraySpec.from_array(a, dimension_names=["x", "y"]) for a in arrays
+#         ],
+#         axes=(Axis(name="x", type="space"), Axis(name="y", type="space")),
+#         paths=arrays_names,
+#         scales=((1, 1), (2, 2)),
+#         translations=((0, 0), (0.5, 0.5)),
+#     )
+#
+#     # make an untyped model, and remove an array before serializing
+#     removed_array_path = arrays_names[0]
+#     model_dict = group_model.model_dump(exclude={"members": {removed_array_path: True}})
+#     broken_group = GroupSpec(**model_dict).to_zarr(store=store, path=group_path)
+#
+#     # put a group where the array should be
+#     broken_group.create_group(removed_array_path)
+#     match = "Invalid value for 'node_type'. Expected 'array'. Got 'group"
+#     with pytest.raises(ValueError, match=match):
+#         Image.from_zarr(broken_group)
+#
+#
+# @pytest.mark.skip
+# def test_hashable(default_multiscale: Multiscale) -> None:
+#     """
+#     Test that `Multiscale` can be hashed
+#     """
+#     assert set(default_multiscale) == set(default_multiscale)
