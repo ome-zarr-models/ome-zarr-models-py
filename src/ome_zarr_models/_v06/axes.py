4487bc8022778d0495824a3377d7409befcbff6e
David Stansby <dstansby@gmail.com>
2025-09-04 10:53:13 +0100

    Changes to RFC4  (#264)
    * Update orientation field name
    
    * Check that orientation can only be set on spatial axes
    
    * Use new orientation data structure

--- src/ome_zarr_models/_v06/axes.py
+++ src/ome_zarr_models/_v06/axes.py
@@ -1,5 +1,7 @@
 from collections.abc import Sequence
-from typing import Literal
+from typing import Self
+
+from pydantic import BaseModel, JsonValue, model_validator
 
 from ome_zarr_models.common.axes import Axis as BaseAxis
 from ome_zarr_models.common.axes import AxisType
@@ -7,34 +9,30 @@ from ome_zarr_models.common.axes import AxisType
 __all__ = ["Axes", "Axis", "AxisType"]
 
 
-Orientation = Literal[
-    "left-to-right",
-    "right-to-left",
-    "anterior-to-posterior",
-    "posterior-to-anterior",
-    "inferior-to-superior",
-    "superior-to-inferior",
-    "dorsal-to-ventral",
-    "ventral-to-dorsal",
-    "dorsal-to-palmar",
-    "palmar-to-dorsal",
-    "dorsal-to-plantar",
-    "plantar-to-dorsal",
-    "rostral-to-caudal",
-    "caudal-to-rostral",
-    "cranial-to-caudal",
-    "caudal-to-cranial",
-    "proximal-to-distal",
-    "distal-to-proximal",
-]
+class Orientation(BaseModel):
+    """
+    Model for an orientation object.
+    """
+
+    type: JsonValue
+    value: JsonValue
 
 
 class Axis(BaseAxis):
     """
-    Model for an element of multiscale axes.
+    Model for an element of `Multiscale.axes`.
     """
 
-    anatomicalOrientation: Orientation | None = None
+    orientation: Orientation | None = None
+
+    @model_validator(mode="after")
+    def _check_orientation_only_on_spatial(self) -> Self:
+        if self.type != "space" and self.orientation is not None:
+            raise ValueError(
+                f"Orientation can only be set on a spatial axis "
+                f"(got Axis type='{self.type}')"
+            )
+        return self
 
 
 Axes = Sequence[Axis]
