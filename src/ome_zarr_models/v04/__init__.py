d47d7be8266751c45ea610134c3a39742bcfc9fe
Talley Lambert <talley.lambert@gmail.com>
2025-09-13 11:57:34 -0400

    add plate

--- src/ome_zarr_models/v04/__init__.py
+++ src/ome_zarr_models/v04/__init__.py
@@ -3,6 +3,7 @@ from ome_zarr_models.v04.hcs import HCS
 from ome_zarr_models.v04.image import Image
 from ome_zarr_models.v04.image_label import ImageLabel
 from ome_zarr_models.v04.labels import Labels
+from ome_zarr_models.v04.plate import Plate
 from ome_zarr_models.v04.well import Well
 
-__all__ = ["HCS", "BaseGroupv04", "Image", "ImageLabel", "Labels", "Well"]
+__all__ = ["HCS", "BaseGroupv04", "Image", "ImageLabel", "Labels", "Plate", "Well"]
