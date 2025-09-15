047056646cceed32bc6ef29ba90ef30928842250
Talley Lambert <talley.lambert@gmail.com>
2025-09-15 13:10:31 -0400

    Update src/ome_zarr_models/cli.py
    Co-authored-by: David Stansby <dstansby@gmail.com>

--- src/ome_zarr_models/cli.py
+++ src/ome_zarr_models/cli.py
@@ -24,7 +24,7 @@ def main() -> None:
     validate_cmd.add_argument("path", type=str, help="Path to OME-Zarr group to validate")
 
     # info sub-command
-    info_cmd = subparsers.add_parser("info", help="Get information about an OME-Zarr")
+    info_cmd = subparsers.add_parser("info", help="Get information about an OME-Zarr group")
     info_cmd.add_argument(
         "path", type=str, help="Path to OME-Zarr group to get information about"
     )
