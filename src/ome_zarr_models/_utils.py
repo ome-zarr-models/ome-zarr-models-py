eed771e20ac375ce9152b66a07c95a85605aa8b3
David Stansby <dstansby@gmail.com>
2025-09-16 13:23:29 +0100

    Add _from_zarr_v3

--- src/ome_zarr_models/_utils.py
+++ src/ome_zarr_models/_utils.py
@@ -2,33 +2,46 @@
 Private utilities.
 """
 
+from __future__ import annotations
+
 from collections import Counter
-from collections.abc import Hashable, Iterable
 from dataclasses import MISSING, fields, is_dataclass
 from typing import TYPE_CHECKING, Any, TypeVar
 
 import pydantic
-import zarr
 from pydantic import create_model
 from pydantic_zarr.v2 import GroupSpec as GroupSpecv2
-from zarr.abc.store import Store
+from pydantic_zarr.v3 import GroupSpec as GroupSpecv3
 
-from ome_zarr_models.base import BaseAttrsv2
+from ome_zarr_models.base import BaseAttrsv2, BaseAttrsv3
 from ome_zarr_models.common.validation import (
     check_array_path,
     check_group_path,
 )
-from ome_zarr_models.v04.base import BaseGroupv04
 
 if TYPE_CHECKING:
+    from collections.abc import Hashable, Iterable
+
+    import zarr
     from pydantic_zarr.v2 import (
         AnyArraySpec as AnyArraySpecv2,
     )
     from pydantic_zarr.v2 import (
         AnyGroupSpec as AnyGroupSpecv2,
     )
+    from pydantic_zarr.v3 import (
+        AnyArraySpec as AnyArraySpecv3,
+    )
+    from pydantic_zarr.v3 import (
+        AnyGroupSpec as AnyGroupSpecv3,
+    )
+    from zarr.abc.store import Store
 
-TBaseGroupv2 = TypeVar("TBaseGroupv2", bound=BaseGroupv04[Any])
+    from ome_zarr_models._v06.base import BaseGroupv06
+    from ome_zarr_models.v04.base import BaseGroupv04
+    from ome_zarr_models.v05.base import BaseGroupv05
+
+TBaseGroupv2 = TypeVar("TBaseGroupv2", bound="BaseGroupv04[Any]")
 TAttrsv2 = TypeVar("TAttrsv2", bound=BaseAttrsv2)
 
 
@@ -92,6 +105,73 @@ def _from_zarr_v2(
             members_tree_flat["/" + group_path + path] = group_flat[path]
 
     members_normalized: AnyGroupSpecv2 = GroupSpecv2.from_flat(members_tree_flat)
+    return group_cls(members=members_normalized.members, attributes=attributes)
+
+
+TBaseGroupv3 = TypeVar("TBaseGroupv3", bound="BaseGroupv05[Any] | BaseGroupv06[Any]")
+TAttrsv3 = TypeVar("TAttrsv3", bound=BaseAttrsv3)
+
+
+def _from_zarr_v3(
+    group: zarr.Group,
+    group_cls: type[TBaseGroupv3],
+    attrs_cls: type[TAttrsv3],
+) -> TBaseGroupv3:
+    """
+    Create a GroupSpec from a potentially unlistable Zarr group.
+
+    This uses methods on the attribute class to get required and optional
+    paths to ararys and groups, and then manually constructs the GroupSpec
+    from those paths.
+
+    Parameters
+    ----------
+    group :
+        Zarr group to create GroupSpec from.
+    group_cls :
+        Class of the Group to return.
+    attrs_cls :
+        Attributes class.
+    """
+    # on unlistable storage backends, the members of this group will be {}
+    group_spec: AnyGroupSpecv3 = GroupSpecv3.from_zarr(group, depth=0)
+    attributes = attrs_cls.model_validate(group_spec.attributes)
+
+    members_tree_flat: dict[str, AnyGroupSpecv3 | AnyArraySpecv3] = {}
+
+    # Required array paths
+    for array_path in attrs_cls.get_array_paths(attributes):
+        array_spec = check_array_path(group, array_path, expected_zarr_version=3)
+        members_tree_flat["/" + array_path] = array_spec
+
+    # Optional array paths
+    for array_path in attrs_cls.get_optional_array_paths(attributes):
+        try:
+            array_spec = check_array_path(group, array_path, expected_zarr_version=3)
+        except ValueError:
+            continue
+        members_tree_flat["/" + array_path] = array_spec
+
+    # Required group paths
+    required_groups = attrs_cls.get_group_paths(attributes)
+    for group_path in required_groups:
+        group_spec = check_group_path(group, group_path, expected_zarr_version=3)
+        group_flat = required_groups[group_path].from_zarr(group[group_path]).to_flat()  # type: ignore[arg-type]
+        for path in group_flat:
+            members_tree_flat["/" + group_path + path] = group_flat[path]
+
+    # Optional group paths
+    optional_groups = attrs_cls.get_optional_group_paths(attributes)
+    for group_path in optional_groups:
+        try:
+            group_spec = check_group_path(group, group_path, expected_zarr_version=3)
+        except FileNotFoundError:
+            continue
+        group_flat = optional_groups[group_path].from_zarr(group[group_path]).to_flat()  # type: ignore[arg-type]
+        for path in group_flat:
+            members_tree_flat["/" + group_path + path] = group_flat[path]
+
+    members_normalized: AnyGroupSpecv3 = GroupSpecv3.from_flat(members_tree_flat)
     return group_cls(members=members_normalized.members, attributes=attributes)
 
 
