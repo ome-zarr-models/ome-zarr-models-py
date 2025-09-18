ac9722034e69848a8b67e34dad767f89d30730f8
Talley Lambert <talley.lambert@gmail.com>
2025-09-18 13:40:19 +0200

    zarr optional

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, cast
 
 import pytest
-import zarr
 from zarr.storage import LocalStore
 
 from ome_zarr_models._cli import main
@@ -14,6 +13,8 @@ if TYPE_CHECKING:
     from collections.abc import Mapping, Sequence
     from pathlib import Path
     from typing import Any
+
+    import zarr
 
 
 def populate_fake_data(
@@ -83,7 +84,7 @@ def test_cli_validate(
     capsys: pytest.CaptureFixture[str],
 ) -> None:
     """Test the CLI commands."""
-
+    pytest.importorskip("zarr")
     zarr_group = json_to_zarr_group(
         version=version, json_fname=json_fname, store=LocalStore(root=tmp_path)
     )
@@ -102,6 +103,7 @@ def test_cli_invalid(
     capsys: pytest.CaptureFixture[str],
 ) -> None:
     """Test the CLI with no command."""
+    zarr = pytest.importorskip("zarr")
     zarr.create_group(tmp_path)
     monkeypatch.setattr("sys.argv", ["ome-zarr-models", cmd, str(tmp_path)])
     with pytest.raises(SystemExit) as excinfo:
