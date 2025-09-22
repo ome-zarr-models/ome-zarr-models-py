1e69b8d6062fd6b9149ccb20179560019633fd35
David Stansby <dstansby@gmail.com>
2025-09-22 17:09:32 +0100

    Allow abitrary StoreLike to open_ome_zarr

--- src/ome_zarr_models/__init__.py
+++ src/ome_zarr_models/__init__.py
@@ -2,6 +2,7 @@ from importlib.metadata import PackageNotFoundError, version
 from typing import TYPE_CHECKING, Any, Literal
 
 import zarr
+import zarr.storage
 
 import ome_zarr_models.v04.hcs
 import ome_zarr_models.v04.image
@@ -54,7 +55,9 @@ _V05_groups: list[type[BaseGroupv05[Any]]] = [
 
 
 def open_ome_zarr(
-    group: zarr.Group, *, version: Literal["0.4", "0.5"] | None = None
+    group: zarr.Group | zarr.storage.StoreLike,
+    *,
+    version: Literal["0.4", "0.5"] | None = None,
 ) -> BaseGroup:
     """
     Create an ome-zarr-models object from an existing OME-Zarr group.
@@ -68,8 +71,9 @@ def open_ome_zarr(
 
     Parameters
     ----------
-    group : zarr.Group
-        Zarr group containing OME-Zarr data.
+    group : zarr.Group, zarr.storage.StoreLike
+        Zarr group containing OME-Zarr data. Alternatively any object that can be
+        parsed by [zarr.open_group][].
     version : Literal['0.4', '0.5'], optional
         If you know which version of OME-Zarr your data is, you can
         specify it here. If not specified, all versions will be tried.
@@ -87,6 +91,9 @@ def open_ome_zarr(
     take a long time. It will be quicker to directly use the OME-Zarr group class if you
     know which version and group you expect.
     """
+    if not isinstance(group, zarr.Group):
+        group = zarr.open_group(group)
+
     # because 'from_zarr' isn't defined on a shared super-class, list all variants here
     groups: Sequence[type[BaseGroupv05[Any] | BaseGroupv04[Any]]]
     match version:
