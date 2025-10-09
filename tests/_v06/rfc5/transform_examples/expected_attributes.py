0961fcba5a06fd0db1c688aac315b49152addf82
David Stansby <dstansby@gmail.com>
2025-10-09 17:50:32 +0100

    Get two transform metadata examples working (#318)
    * Get two of the RFC5 examples working.
    
    * Error if list out of date
    
    * Put back xfail

--- tests/_v06/rfc5/transform_examples/expected_attributes.py
+++
@@ -1,6 +0,0 @@
-from collections import defaultdict
-from pathlib import Path
-
-from ome_zarr_models._v06.image import ImageAttrs
-
-expected_attrs: dict[Path, None | ImageAttrs] = defaultdict(lambda: None)
