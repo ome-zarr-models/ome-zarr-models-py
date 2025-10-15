ab184925abb9de71a4aec0ce9486eb7119d61dc0
David Stansby <dstansby@gmail.com>
2025-10-15 11:43:09 +0100

    Re-implement Image.new() for RFC-5 (#322)
    * Re-implement Image.new() for RFC 5
    
    * Set discrete
    
    * Add translation to test

--- tests/_v06/test_image.py
+++ tests/_v06/test_image.py
@@ -1,9 +1,12 @@
+from pydantic_zarr.v3 import AnyArraySpec, ArraySpec, NamedConfig
 from zarr.abc.store import Store
 
 from ome_zarr_models._v06.coordinate_transforms import (
     Axis,
     CoordinateSystem,
     Scale,
+    Sequence,
+    Translation,
 )
 from ome_zarr_models._v06.image import Image, ImageAttrs
 from ome_zarr_models._v06.multiscales import Dataset, Multiscale
@@ -123,3 +126,197 @@ def test_image(store: Store) -> None:
     )
     print(image_attrs.model_dump_json(indent=4))
     assert ome_group.attributes.ome == image_attrs
+
+
+def test_image_new() -> None:
+    # Spec for the full resolution data
+    array_spec: AnyArraySpec = ArraySpec(
+        shape=(100, 100),
+        data_type="uint16",
+        chunk_grid=NamedConfig(
+            name="regular",
+            configuration={"chunk_shape": [32, 32]},
+        ),
+        chunk_key_encoding=NamedConfig(
+            name="default", configuration={"separator": "/"}
+        ),
+        fill_value=0,
+        codecs=[NamedConfig(name="bytes")],
+        dimension_names=["y", "x"],
+    )
+
+    array_specs: list[AnyArraySpec] = [
+        # Full res
+        array_spec,
+        # Half-res
+        array_spec.model_copy(update={"shape": (50, 50)}),
+    ]
+
+    image = Image.new(
+        array_specs=array_specs,
+        paths=["0", "1"],
+        scales=[[1, 1], [2, 2]],
+        translations=[[0, 0], [1, 1]],
+        name="my_image",
+        output_coord_transform=Scale(
+            scale=(
+                0.5,
+                0.2,
+            )
+        ),
+        output_coord_system=CoordinateSystem(
+            name="my_image_coords",
+            axes=(
+                Axis(name="y", type="space", unit="micrometer", discrete=False),
+                Axis(name="x", type="space", unit="micrometer", discrete=False),
+            ),
+        ),
+    )
+    assert image.members == {
+        "0": ArraySpec(
+            zarr_format=3,
+            node_type="array",
+            attributes={},
+            shape=(100, 100),
+            data_type="uint16",
+            chunk_grid={"name": "regular", "configuration": {"chunk_shape": (32, 32)}},
+            chunk_key_encoding={"name": "default", "configuration": {"separator": "/"}},
+            fill_value=0,
+            codecs=({"name": "bytes"},),
+            storage_transformers=(),
+            dimension_names=("y", "x"),
+        ),
+        "1": ArraySpec(
+            zarr_format=3,
+            node_type="array",
+            attributes={},
+            shape=(50, 50),
+            data_type="uint16",
+            chunk_grid={"name": "regular", "configuration": {"chunk_shape": (32, 32)}},
+            chunk_key_encoding={"name": "default", "configuration": {"separator": "/"}},
+            fill_value=0,
+            codecs=({"name": "bytes"},),
+            storage_transformers=(),
+            dimension_names=("y", "x"),
+        ),
+    }
+    assert image.ome_attributes == ImageAttrs(
+        version="0.6",
+        multiscales=[
+            Multiscale(
+                coordinateSystems=(
+                    CoordinateSystem(
+                        name="my_image_array_coords",
+                        axes=(
+                            Axis(
+                                name="y",
+                                type="array",
+                                discrete=True,
+                                unit=None,
+                                longName=None,
+                            ),
+                            Axis(
+                                name="x",
+                                type="array",
+                                discrete=True,
+                                unit=None,
+                                longName=None,
+                            ),
+                        ),
+                    ),
+                    CoordinateSystem(
+                        name="my_image_coords",
+                        axes=(
+                            Axis(
+                                name="y",
+                                type="space",
+                                discrete=False,
+                                unit="micrometer",
+                                longName=None,
+                            ),
+                            Axis(
+                                name="x",
+                                type="space",
+                                discrete=False,
+                                unit="micrometer",
+                                longName=None,
+                            ),
+                        ),
+                    ),
+                ),
+                datasets=(
+                    Dataset(
+                        path="0",
+                        coordinateTransformations=(
+                            Sequence(
+                                type="sequence",
+                                input="0",
+                                output="my_image_array_coords",
+                                name=None,
+                                transformations=(
+                                    Scale(
+                                        type="scale",
+                                        input=None,
+                                        output=None,
+                                        name=None,
+                                        scale=(1.0, 1.0),
+                                        path=None,
+                                    ),
+                                    Translation(
+                                        type="translation",
+                                        input=None,
+                                        output=None,
+                                        name=None,
+                                        translation=(0.0, 0.0),
+                                        path=None,
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+                    Dataset(
+                        path="1",
+                        coordinateTransformations=(
+                            Sequence(
+                                type="sequence",
+                                input="1",
+                                output="my_image_array_coords",
+                                name=None,
+                                transformations=(
+                                    Scale(
+                                        type="scale",
+                                        input=None,
+                                        output=None,
+                                        name=None,
+                                        scale=(2.0, 2.0),
+                                        path=None,
+                                    ),
+                                    Translation(
+                                        type="translation",
+                                        input=None,
+                                        output=None,
+                                        name=None,
+                                        translation=(1.0, 1.0),
+                                        path=None,
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+                coordinateTransformations=(
+                    Scale(
+                        type="scale",
+                        input="my_image_array_coords",
+                        output="my_image_coords",
+                        name=None,
+                        scale=(0.5, 0.2),
+                        path=None,
+                    ),
+                ),
+                metadata=None,
+                name="my_image",
+                type=None,
+            )
+        ],
+    )
