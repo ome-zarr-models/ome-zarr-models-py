70b81a1749b6128cbd1f1a3106465e2037c915e1
David Stansby <dstansby@gmail.com>
2025-10-15 19:00:18 +0100

    Parse child arrays

--- src/ome_zarr_models/_v06/container.py
+++ src/ome_zarr_models/_v06/container.py
@@ -1,6 +1,7 @@
 from typing import Self
 
 import zarr
+from pydantic import Field, PrivateAttr
 
 from ome_zarr_models._utils import _from_zarr_v3
 from ome_zarr_models._v06.base import BaseGroupv06, BaseOMEAttrs
@@ -9,17 +10,35 @@ from ome_zarr_models._v06.coordinate_transforms import (
     CoordinateSystem,
     Transform,
 )
+from ome_zarr_models._v06.image import Image
 
 
 class ContainerAttrs(BaseOMEAttrs):
-    coordinateTransformations: tuple[AnyTransform, ...]
-    coordinateSystems: tuple[CoordinateSystem, ...]
+    coordinateTransformations: tuple[AnyTransform, ...] = Field(default=())
+    coordinateSystems: tuple[CoordinateSystem, ...] = Field(default=())
+
+    def get_group_paths(self) -> dict[str, type[Image]]:  # type: ignore[override]
+        paths = {}
+        for transform in self.coordinateTransformations:
+            if transform.input is not None and transform.input.startswith("/"):
+                paths[transform.input.removeprefix("/")] = Image
+
+        return paths
 
 
 class Container(BaseGroupv06[ContainerAttrs]):
     """
-    An OME-Zarr image dataset.
+    An OME-Zarr container group.
+
+    This is a group that stores a series of other multiscale images,
+    along with additional coordinate transformations and coordinate systems.
     """
+
+    _images: list[Image] = PrivateAttr(default=[])
+    # List of all coordinate systems found within this container
+    _all_systems: list[CoordinateSystem] = PrivateAttr(default=[])
+    # List of all coordinate transformations found within this container
+    _all_transforms: list[AnyTransform] = PrivateAttr(default=[])
 
     @classmethod
     def from_zarr(cls, group: zarr.Group) -> Self:  # type: ignore[override]
@@ -31,4 +50,21 @@ class Container(BaseGroupv06[ContainerAttrs]):
         group : zarr.Group
             A Zarr group that has valid OME-Zarr image metadata.
         """
-        return _from_zarr_v3(group, cls, ContainerAttrs)
+        self = _from_zarr_v3(group, cls, ContainerAttrs)
+        self._all_systems.extend(self.ome_attributes.coordinateSystems)
+        self._all_transforms.extend(self.ome_attributes.coordinateTransformations)
+        return self
+
+    @property
+    def coordinate_systems(self) -> tuple[CoordinateSystem, ...]:
+        """
+        All coordinate systems in this container.
+        """
+        return tuple(self._all_systems)
+
+    @property
+    def coordinate_transforms(self) -> tuple[Transform, ...]:
+        """
+        All coordinate transformations in this container.
+        """
+        return tuple(self._all_transforms)
--- tests/_v06/test_container.py
+++ tests/_v06/test_container.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
 import zarr
+from pydantic_zarr.v3 import ArraySpec, GroupSpec
 
 from ome_zarr_models._v06.container import Container, ContainerAttrs
 from ome_zarr_models._v06.coordinate_transforms import (
     Axis,
     CoordinateSystem,
-    Transform,
     Translation,
 )
 
@@ -20,7 +20,168 @@ def test_load_container() -> None:
         / "stitched_tiles_2d.zarr"
     )
     container = Container.from_zarr(group)
-    assert container.members == {}
+    assert container.members == {
+        "tile_0": GroupSpec(
+            zarr_format=3,
+            node_type="group",
+            attributes={
+                "ome": {
+                    "version": "0.6.dev1",
+                    "multiscales": [
+                        {
+                            "coordinateSystems": (
+                                {
+                                    "name": "physical",
+                                    "axes": (
+                                        {
+                                            "name": "y",
+                                            "type": "space",
+                                            "discrete": False,
+                                            "unit": "micrometer",
+                                            "longName": None,
+                                        },
+                                        {
+                                            "name": "x",
+                                            "type": "space",
+                                            "discrete": False,
+                                            "unit": "micrometer",
+                                            "longName": None,
+                                        },
+                                    ),
+                                },
+                            ),
+                            "datasets": (
+                                {
+                                    "path": "0",
+                                    "coordinateTransformations": (
+                                        {
+                                            "type": "scale",
+                                            "input": "0",
+                                            "output": "physical",
+                                            "name": "tile_0 to physical",
+                                            "scale": (1.0, 1.0),
+                                            "path": None,
+                                        },
+                                    ),
+                                },
+                            ),
+                            "coordinateTransformations": None,
+                            "metadata": None,
+                            "name": "multiscales",
+                            "type": None,
+                        }
+                    ],
+                }
+            },
+            members={
+                "0": ArraySpec(
+                    zarr_format=3,
+                    node_type="array",
+                    attributes={},
+                    shape=(300, 372),
+                    data_type="uint8",
+                    chunk_grid={
+                        "name": "regular",
+                        "configuration": {"chunk_shape": (300, 372)},
+                    },
+                    chunk_key_encoding={
+                        "name": "default",
+                        "configuration": {"separator": "/"},
+                    },
+                    fill_value=0,
+                    codecs=(
+                        {"name": "bytes"},
+                        {
+                            "name": "zstd",
+                            "configuration": {"level": 3, "checksum": False},
+                        },
+                    ),
+                    storage_transformers=(),
+                    dimension_names=None,
+                )
+            },
+        ),
+        "tile_1": GroupSpec(
+            zarr_format=3,
+            node_type="group",
+            attributes={
+                "ome": {
+                    "version": "0.6.dev1",
+                    "multiscales": [
+                        {
+                            "coordinateSystems": (
+                                {
+                                    "name": "physical",
+                                    "axes": (
+                                        {
+                                            "name": "y",
+                                            "type": "space",
+                                            "discrete": False,
+                                            "unit": "micrometer",
+                                            "longName": None,
+                                        },
+                                        {
+                                            "name": "x",
+                                            "type": "space",
+                                            "discrete": False,
+                                            "unit": "micrometer",
+                                            "longName": None,
+                                        },
+                                    ),
+                                },
+                            ),
+                            "datasets": (
+                                {
+                                    "path": "0",
+                                    "coordinateTransformations": (
+                                        {
+                                            "type": "scale",
+                                            "input": "0",
+                                            "output": "physical",
+                                            "name": "tile_1 to physical",
+                                            "scale": (1.0, 1.0),
+                                            "path": None,
+                                        },
+                                    ),
+                                },
+                            ),
+                            "coordinateTransformations": None,
+                            "metadata": None,
+                            "name": "multiscales",
+                            "type": None,
+                        }
+                    ],
+                }
+            },
+            members={
+                "0": ArraySpec(
+                    zarr_format=3,
+                    node_type="array",
+                    attributes={},
+                    shape=(300, 372),
+                    data_type="uint8",
+                    chunk_grid={
+                        "name": "regular",
+                        "configuration": {"chunk_shape": (300, 372)},
+                    },
+                    chunk_key_encoding={
+                        "name": "default",
+                        "configuration": {"separator": "/"},
+                    },
+                    fill_value=0,
+                    codecs=(
+                        {"name": "bytes"},
+                        {
+                            "name": "zstd",
+                            "configuration": {"level": 3, "checksum": False},
+                        },
+                    ),
+                    storage_transformers=(),
+                    dimension_names=None,
+                )
+            },
+        ),
+    }
     assert container.ome_attributes == ContainerAttrs(
         version="0.6",
         coordinateTransformations=(
@@ -59,5 +220,46 @@ def test_load_container() -> None:
                     ),
                 ),
             ),
+        ),
+    )
+
+    assert container.coordinate_systems == (
+        CoordinateSystem(
+            name="world",
+            axes=(
+                Axis(
+                    name="x",
+                    type="space",
+                    discrete=False,
+                    unit="micrometer",
+                    longName=None,
+                ),
+                Axis(
+                    name="y",
+                    type="space",
+                    discrete=False,
+                    unit="micrometer",
+                    longName=None,
+                ),
+            ),
+        ),
+    )
+
+    assert container.coordinate_transforms == (
+        Translation(
+            type="translation",
+            input="/tile_0",
+            output="world",
+            name="tile_0_mm to world",
+            translation=(0.0, 0.0),
+            path=None,
+        ),
+        Translation(
+            type="translation",
+            input="/tile_1",
+            output="world",
+            name="tile_1_mm to world",
+            translation=(0.0, 348.0),
+            path=None,
         ),
     )
