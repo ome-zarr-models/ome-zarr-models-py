b8fa724b31a511188c73b50fa1b73693cd9ecd34
David Stansby <dstansby@gmail.com>
2025-10-22 00:14:04 +0200

    Add basic container for loading RFC-5 parent groups (#327)
    * Add basic container loading
    
    * Parse child arrays
    
    * Remove padding
    
    * Use container in tests
    
    * container > collection

---
+++ src/ome_zarr_models/_v06/collection.py
@@ -0,0 +1,46 @@
+from typing import Self
+
+import zarr
+from pydantic import Field
+
+from ome_zarr_models._utils import _from_zarr_v3
+from ome_zarr_models._v06.base import BaseGroupv06, BaseOMEAttrs
+from ome_zarr_models._v06.coordinate_transforms import (
+    AnyTransform,
+    CoordinateSystem,
+)
+from ome_zarr_models._v06.image import Image
+
+
+class CollectionAttrs(BaseOMEAttrs):
+    coordinateTransformations: tuple[AnyTransform, ...] = Field(default=())
+    coordinateSystems: tuple[CoordinateSystem, ...] = Field(default=())
+
+    def get_group_paths(self) -> dict[str, type[Image]]:  # type: ignore[override]
+        paths = {}
+        for transform in self.coordinateTransformations:
+            if transform.input is not None and transform.input.startswith("/"):
+                paths[transform.input.removeprefix("/")] = Image
+
+        return paths
+
+
+class Collection(BaseGroupv06[CollectionAttrs]):
+    """
+    An OME-Zarr container group.
+
+    This is a group that stores a series of other multiscale images,
+    along with additional coordinate transformations and coordinate systems.
+    """
+
+    @classmethod
+    def from_zarr(cls, group: zarr.Group) -> Self:  # type: ignore[override]
+        """
+        Create an OME-Zarr collection from a `zarr.Group`.
+
+        Parameters
+        ----------
+        group : zarr.Group
+            A Zarr group that has valid OME-Zarr image metadata.
+        """
+        return _from_zarr_v3(group, cls, CollectionAttrs)
