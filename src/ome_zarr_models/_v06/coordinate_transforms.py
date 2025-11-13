d059fc2a6b2c8f9c1087d659816f3a6b587141f7
Luca Marconato <m.lucalmer@gmail.com>
2025-11-13 15:45:45 +0100

    add cs identifier for subgraphs

--- src/ome_zarr_models/_v06/coordinate_transforms.py
+++ src/ome_zarr_models/_v06/coordinate_transforms.py
@@ -60,6 +60,13 @@ class CoordinateSystem(BaseAttrs):
     def ndim(self) -> int:
         return len(self.axes)
 
+class CoordinateSystemIdentifier(BaseAttrs):
+    """
+    Model for a coordinate system identifier.
+    """
+
+    name: str
+    path: str
 
 class Transform(BaseAttrs, ABC):
     """
@@ -76,8 +83,8 @@ class Transform(BaseAttrs, ABC):
     """
 
     type: str
-    input: str | None = None
-    output: str | None = None
+    input: str | CoordinateSystemIdentifier | None = None
+    output: str | CoordinateSystemIdentifier | None = None
     name: str | None = None
 
     @model_validator(mode="after")
