56ce3fb3c880c415a7bd99c2611caf763c5fbd05
David Stansby <dstansby@gmail.com>
2025-11-13 12:10:35 +0100

    Add code to transform points (#314)
    * Add code to transform points
    
    * Add inverse transform method
    
    * Add some inverse transform tests
    
    * Implement affine transform
    
    * Validate row lengths in Affine
    
    * Add a point type
    
    * Add method to get inverse transforms
    
    * Remove inverse_transform_point
    
    * Clean up old comment
    
    * Add notes about transform_point impelmentation
    
    * Add test for affine dimension mismatch
    
    * Implement rotation transform
    
    * Test sequence transform
    
    * Fixes to read in metadata examples
    
    * Remove duplicate property
    
    * Remove old method

--- tests/_v06/test_coordinate_transforms.py
+++ tests/_v06/test_coordinate_transforms.py
@@ -1,3 +1,5 @@
+import re
+
 import pytest
 from pydantic import ValidationError
 
@@ -58,3 +60,70 @@ def test_inverse(transform: Transform, inverse_expected: Transform) -> None:
         update={"input": "output_system", "output": "input_system"}
     )
     assert transform.get_inverse() == inverse_expect
+
+
+@pytest.mark.parametrize(
+    "transform, expected_point",
+    (
+        (Identity(), (0, 1, 2)),
+        (Translation(translation=(3.2, 10.2, 7.5)), (3.2, 11.2, 9.5)),
+        (Scale(scale=(1, 0.5, 2)), (0, 0.5, 4)),
+        (Affine(affine=((1, 0, 0, 10), (0, 1, 0, -4), (0, 0, 1, 2))), (10, -3, 4)),
+        # TODO: change the rotation matrix to not the identity
+        (Rotation(rotation=((1, 0, 0), (0, 1, 0), (0, 0, 1))), (0, 1, 2)),
+        (
+            Sequence(
+                transformations=(
+                    Scale(scale=(1, 0.5, 2)),
+                    Translation(translation=(0, 1, 2)),
+                )
+            ),
+            (0, 1.5, 6),
+        ),
+    ),
+)
+def test_transform_point(
+    transform: Transform, expected_point: tuple[int, int, int]
+) -> None:
+    """
+    Test transforming a single point.
+    """
+    actual_point = transform.transform_point((0, 1, 2))
+    assert actual_point == expected_point
+
+
+@pytest.mark.parametrize(
+    "transform, expected_point",
+    (
+        (Identity(), (0, 1, 2)),
+        (Translation(translation=(3.2, 10.2, 7.5)), (-3.2, -9.2, -5.5)),
+        (Scale(scale=(1, 0.5, 2)), (0, 2, 1)),
+    ),
+)
+def test_inverse_transform_point(
+    transform: Transform, expected_point: tuple[int, int, int]
+) -> None:
+    """
+    Test transforming a single point.
+    """
+    actual_point = transform.get_inverse().transform_point((0, 1, 2))
+    assert actual_point == expected_point
+
+
+def test_invalid_affine() -> None:
+    with pytest.raises(
+        ValidationError,
+        match=re.escape("Row lengths in affine matrix ([1, 2]) are not all equal."),
+    ):
+        Affine(affine=((1,), (1, 2)))
+
+
+def test_affine_dimension_mismatch() -> None:
+    t = Affine(affine=((1, 0), (0, 1)))
+    with pytest.raises(
+        ValueError,
+        match=re.escape(
+            "Dimensionality of point (1) does not match dimensionality of transform (2)"
+        ),
+    ):
+        t.transform_point((1,))
