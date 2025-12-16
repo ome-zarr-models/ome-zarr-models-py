1bb03089adda76a112618c2181e73b698c2632f5
David Stansby <dstansby@gmail.com>
2025-12-16 11:32:43 +0000

    Support bioformats2raw in v04

---
+++ src/ome_zarr_models/v05/bioformats2raw.py
@@ -0,0 +1,20 @@
+from ome_zarr_models.v05.base import BaseGroupv05, BaseOMEAttrs
+from pydantic import Field
+
+
+class Bioformats2rawAttrs(BaseOMEAttrs):
+    """
+    Attributes for an OME-Zarr bioformats2raw dataset.
+    """
+
+    labels: list[str] = Field(
+        ..., description="List of paths to labels arrays within a labels dataset."
+    )
+
+
+class Bioformats2raw(
+    BaseGroupv05[Bioformats2rawAttrs],
+):
+    """
+    An OME-Zarr labels dataset.
+    """
