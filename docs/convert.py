1e242b12d804e2fc4a6b1a4524280f5a28652fd2
Johannes Soltwedel <38459088+jo-mueller@users.noreply.github.com>
2026-05-26 14:53:54 +0200

    docs: Explain how to fill/edit fields after conversion (#451)
    * docs: Explain how to fill/edit fields after conversion
    
    * chore: precommit fix
    
    * docs: use validated update in docs
    
    * Fix pre-commit
    
    ---------
    
    Co-authored-by: David Stansby <dstansby@gmail.com>

--- docs/convert.py
+++ docs/convert.py
@@ -53,3 +53,20 @@ print(multiscale_v06)
 
 multiscale_v04 = multiscale.to_version("0.4")
 print(multiscale_v04)
+
+# The 0.6 specification provides some new fields over the 0.5 version,
+# such as the `coordinateSystems` field,
+# which provides named coordinate system references.
+#
+# In the process of conversion, these new fields are provided with default values
+# (i.e., the `name` of the default coordinate system is set to "physical").
+# However, these fields can manually be filled or edited after conversion if desired:
+
+metadata = multiscale_v06.model_dump()
+metadata["coordinateSystems"][0]["axes"][0]["longName"] = "OpticalAxis"
+
+# And we can update the multiscale metadata model with the edits:
+
+edited_multiscale_v06 = ome_zarr_models.v06.multiscales.Multiscale.model_validate(
+    metadata
+)
